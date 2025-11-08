9a771ddc6a593c34295edb6f87a2c1b797ee6fab
W7YFR <331321620+W7YFR@users.noreply.github.com>
2025-11-08 10:59:14 -0800

    feat: config for mega setup

--- k3ng_keyer/keyer_features_and_options.h
+++ k3ng_keyer/keyer_features_and_options.h
@@ -2,23 +2,23 @@
 // FEATURES add more bytes to the compiled binary, OPTIONS change code behavior
 
 
-// #define FEATURE_BUTTONS
-// #define EXTRA_BUTTONS                  // adds one or more analog buttons beyond the command + memory buttons, for custom actions (see check_buttons() in k3ng_keyer.ino) - requires FEATURE_BUTTONS
-// #define NUMBER_OF_EXTRA_BUTTONS 1      // how many extra buttons to add to the button ladder when EXTRA_BUTTONS is defined
-// #define FEATURE_COMMAND_MODE
-// #define FEATURE_COMMAND_LINE_INTERFACE // Command Line Interface functionality
-// #define FEATURE_MEMORIES               // on the Arduino Due, you must have FEATURE_EEPROM_E24C1024 and E24C1024 EEPROM hardware in order to compile this
-// #define FEATURE_MEMORY_MACROS
+#define FEATURE_BUTTONS
+#define FEATURE_COMMAND_MODE
+#define FEATURE_COMMAND_LINE_INTERFACE // Command Line Interface functionality
+#define FEATURE_MEMORIES               // on the Arduino Due, you must have FEATURE_EEPROM_E24C1024 and E24C1024 EEPROM hardware in order to compile this
+#define FEATURE_MEMORY_MACROS
 // #define FEATURE_WINKEY_EMULATION       // disabling Automatic Software Reset is highly recommended (see documentation)
 // #define FEATURE_BEACON                 // Go into beacon mode if paddle_left pin is LOW at boot up
 // #define FEATURE_BEACON_SETTING         // Go into beacon mode at boot up if EEPROM setting is enabled (\_ CLI Command)
-// #define FEATURE_TRAINING_COMMAND_LINE_INTERFACE
+#define FEATURE_TRAINING_COMMAND_LINE_INTERFACE
+#define FEATURE_POTENTIOMETER          // do not enable unless you have a potentiometer connected, otherwise noise will falsely trigger wpm changes
+// #define EXTRA_BUTTONS                  // adds one or more analog buttons beyond the command + memory buttons, for custom actions (see check_buttons() in k3ng_keyer.ino) - requires FEATURE_BUTTONS
+// #define NUMBER_OF_EXTRA_BUTTONS 1      // how many extra buttons to add to the button ladder when EXTRA_BUTTONS is defined
 // #define FEATURE_TRAINING_PADDLE        // makes the word/callsign/QSO training tables available without FEATURE_TRAINING_COMMAND_LINE_INTERFACE, for paddle-only (no serial terminal) echo practice in Command Mode
 // #define OPTION_ECHO_PRACTICE_DOUBLE_CORRECT_AFTER_MISS  // in echo practice, once you miss a word you must get it right twice in a row before moving on to the next word
-// #define FEATURE_POTENTIOMETER          // do not enable unless you have a potentiometer connected, otherwise noise will falsely trigger wpm changes
 // #define FEATURE_SIDETONE_SWITCH        // adds switch control for the sidetone output. requires an external toggle switch (assigned to an arduino pin - see keyer_pin_settings.h). 
 // #define FEATURE_SIDETONE_NEWTONE       // Use the NewTone library, ~1k smaller code size than the standard tone library. Uses timer1 (pins 9 or 10)  https://bitbucket.org/teckel12/arduino-new-tone/wiki/Home
-// #define FEATURE_SERIAL_HELP
+#define FEATURE_SERIAL_HELP
 // #define FEATURE_HELL                   // Hellscreiber Mode
 // #define FEATURE_PS2_KEYBOARD           // Use a PS2 keyboard to send code - Change keyboard layout (non-US) in K3NG_PS2Keyboard.h.  Additional options below.
 // #define FEATURE_USB_KEYBOARD           // Use a USB keyboard to send code - Uncomment three lines in k3ng_keyer.ino (search for note_usb_uncomment_lines)
@@ -40,13 +40,13 @@
 // #define FEATURE_LCD_FABO_PCF8574        // https://github.com/FaBoPlatform/FaBoLCD-PCF8574-Library
 // #define FEATURE_LCD_MATHERTEL_PCF8574   // https://github.com/mathertel/LiquidCrystal_PCF8574
 // #define FEATURE_LCD_I2C_FDEBRABANDER    // https://github.com/fdebrabander/Arduino-LiquidCrystal-I2C-library
-// #define FEATURE_LCD_HD44780             // hd44780_I2Cexp subclass control LCD using i2c i/o expander backpack (PCF8574 or MCP23008) see https://https://github.com/duinoWitchery/hd44780 
+// #define FEATURE_LCD_HD44780
+#define FEATURE_OLED_SSD1306            // https://github.com/greiman/SSD1306Ascii
 // #define FEATURE_LCD_HD44780_PINIO       // hd44780_pinIO subclass control LCD using direct Arduino Pin connections see https://https://github.com/duinoWitchery/hd44780 
-// #define FEATURE_OLED_SSD1306            // https://github.com/greiman/SSD1306Ascii
 // #define FEATURE_CW_DECODER              // https://github.com/k3ng/k3ng_cw_keyer/wiki/385-Feature:-CW-Decoder 
 // #define FEATURE_SLEEP                   // go to sleep after x minutes to conserve battery power (not compatible with Arduino DUE, may have mixed results with Mega and Mega ADK)
 // #define FEATURE_LCD_BACKLIGHT_AUTO_DIM  // turn off LCD backlight and/or dim Power Indicator LED after x minutes (LED requires a PWM pin)
-// #define FEATURE_ROTARY_ENCODER          // rotary encoder speed control
+#define FEATURE_ROTARY_ENCODER          // rotary encoder speed control
 // #define FEATURE_CMOS_SUPER_KEYER_IAMBIC_B_TIMING
 // #define FEATURE_USB_MOUSE               // Uncomment three lines in k3ng_keyer.ino (search for note_usb_uncomment_lines)
 // #define FEATURE_CAPACITIVE_PADDLE_PINS  // remove the bypass capacitors on the paddle_left and paddle_right lines when using capactive paddles
@@ -139,7 +139,7 @@
 
 // #define OPTION_DISABLE_SERIAL_PORT_CHECKING_WHILE_SENDING_CW
 
-// #define OPTION_PERSONALIZED_STARTUP_SCREEN        // displays a user defined string of characters on the second or fourth row of the screen during startup. 1602 display requires OPTION_DO_NOT_SAY_HI
+#define OPTION_PERSONALIZED_STARTUP_SCREEN        // displays a user defined string of characters on the second or fourth row of the screen during startup. 1602 display requires OPTION_DO_NOT_SAY_HI
 // #define OPTION_SWAP_PADDLE_PARAMETER_CHANGE_DIRECTION        // reverses the up/down direction when using the paddles to change the wpm or sidetone frequency
 // #define OPTION_DISPLAY_MEMORY_CONTENTS_COMMAND_MODE
 // #define OPTION_SKIP_SAVED_MEMORY_PLAYBACK // skips playback of the memory after recording it
