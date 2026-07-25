c376d5772a7b81bc9d9175fda664789905f9c75e
W7YFR <331321620+W7YFR@users.noreply.github.com>
2026-07-24 22:58:48 -0700

    feat: clear display after memory

--- k3ng_keyer/k3ng_keyer.ino
+++ k3ng_keyer/k3ng_keyer.ino
@@ -17737,6 +17737,7 @@ void program_memory(int memory_number)
     }
     lcd_print_string.concat(memory_number+1);
     lcd_center_print_timed(lcd_print_string, 0, default_display_msg_delay);
+    display_scroll_reset();
   #endif
 
   //send_dit();
@@ -17870,6 +17871,7 @@ void program_memory(int memory_number)
            boop();
            #ifdef FEATURE_DISPLAY
              lcd_center_print_timed(lcd_print_string, 0, default_display_msg_delay);
+             display_scroll_reset();
            #endif //FEATURE_DISPLAY
          }
        #endif
@@ -17941,6 +17943,7 @@ void program_memory(int memory_number)
 
   #ifdef FEATURE_DISPLAY
     lcd_center_print_timed("Done", 0, default_display_msg_delay);
+    display_scroll_reset();
   #endif
 
   #ifndef OPTION_SKIP_SAVED_MEMORY_PLAYBACK
